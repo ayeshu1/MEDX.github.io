bbe9a8a059ab8f7ba1353e895acb340df9c19fc0
ayeshu1 <heebshaikh@gmail.com>
2022-03-04 19:15:23 +0530

    medx

---
+++ about.php
@@ -0,0 +1,53 @@
+<?php
+
+
+include "header.php";
+
+?>
+
+
+
+<html>
+<body>
+
+<div class="container" style="padding: 50px 0px;">
+  <div class="row align-items-start">
+     <div class="col-sm-12">
+     <center><h1 style="color:#fff;"> About Us</h1></center>  
+    
+    </div>
+</div>
+</div>
+
+
+<div class="container" style="padding: 50px 0px;">
+  <div class="row align-items-start">
+     <div class="col-sm-6">
+       <img src="chemist2.webp" width="550px" height="400px" />
+    <background-color:aqua;
+    </div>
+  </div>
+
+    <div class="col-sm-6">
+      <p> Med-x-drug is an oline medical shop which provides you all medical service in online platform.The main object of the online medicine delevery system is to provide better services to all types of users of this website with trust and occuracy with cheaper costs.Med-x-drug is one of india's most  trusted online medical  shop offering flat 30 off than local medical stores.In our medical shop
+
+ each and every medicine are availaible, By adding hundreds of products in our online store, User can search desired medicines, General medicines and get in thier door step with in one click , So that they can get free delivery with discount on medicines in their door.
+
+. In comming future all over india, Every state medical shops can added here to serve you better in online platform.</p>
+    </div>
+    
+  </div>
+  </div>
+
+	  </body>
+	  </html>
+
+
+
+
+<?php
+
+
+include "footer.php";
+
+?>
---
+++ contact.php
@@ -0,0 +1,62 @@
+<?php
+
+
+include "header.php";
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<style>
+		.fa {
+  padding: 20px;
+  font-size: 25px;
+  width: 30px;
+  text-align: center;
+  text-decoration: none;
+  border-radius: 50%;
+  color: #fff;
+  margin-right: 15px;
+}
+
+	</style>
+<body>
+<div class="mapouter"><div class="gmap_canvas"><iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=900&amp;height=400&amp;hl=en&amp;q=pune&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://www.fnfgo.com/">FNF Mods</a></div><style>.mapouter{position:relative;text-align:right;width:100%;height:400px;}.gmap_canvas {overflow:hidden;background:none!important;width:100%;height:400px;}.gmap_iframe {height:400px!important;}</style></div>
+
+
+
+<div class="container" style="padding: 50px 0px;">
+  <div class="row align-items-start">
+     <div class="col-sm-4">
+       
+       
+       <center><i class="fa fa-envelope"></i> admin@gmail.com</p></center>
+    
+    </div>
+    <div class="col-sm-4">
+     <center> Follow:<br>
+<a href="#" class="fa fa-facebook"></a>
+
+<a href="#" class="fa fa-twitter"></a>
+
+<a href="#" class="fa fa-google"></a>
+
+<a href="#" class="fa fa-linkedin"></a></center>  
+    </div>
+
+<div class="col-sm-4">
+     <center><i class="fa fa-phone"></i> +91365987410</p></center>
+    
+    </div>
+
+</div>
+</div>
+
+</body>
+</html>
+<?php
+
+
+include "footer.php";
+
+?>
